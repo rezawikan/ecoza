97fd82c22287e8863cda735ee027181048357dd2
= <=>
2019-09-02 17:26:23 +0800

    add deep tree recursive

---
+++ components/navigation/tree/MenuTree.vue
@@ -0,0 +1,37 @@
+<template lang="html">
+  <li class="has-children">
+    <span><a href="#">{{ label }}</a><span v-if="child.length > 0" class="sub-menu-toggle" /></span>
+
+    <ul v-if="child.length > 0" class="offcanvas-submenu">
+      <li class="back-btn">
+        <a href="#">Back</a>
+      </li>
+      <MenuDeepTree
+        v-for="(sub, index) in child"
+        :key="index"
+        :label="sub.label"
+        :child="sub.child"
+      />
+    </ul>
+  </li>
+</template>
+
+<script>
+export default {
+  name: 'MenuDeepTree',
+  props: {
+    label: {
+      type: String,
+      default: () => []
+    },
+    child: {
+      type: Array,
+      default: () => []
+    }
+  }
+
+}
+</script>
+
+<style lang="css" scoped>
+</style>
